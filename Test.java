35773aa83b5ebfd0a3b60af77f2003a1407166b4
GadeHasika <160099681+GadeHasika@users.noreply.github.com>
2025-12-03 13:49:48 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,13 +1,13 @@
-// import java.util.*;
-// class Test{
-//     public static void main(String[]args){
-//         try{
-//             if(Factorial(5)!=120) throw new AssertionError("Test failed");
-//             if(Factorial(0)!=1) throw new AssertionError("TestFailed");
-//             System.out.println("Test passed");
-//         }
-//         catch(Exception e){
-//             System.out.println("Exception occured"+ e.getMessage());
-//         }
-//     }
-// }
+import java.util.*;
+class Test{
+    public static void main(String[]args){
+        try{
+            if(Fact.Factorial(5)!=120) throw new AssertionError("Test failed");
+            if(Fact.Factorial(0)!=1) throw new AssertionError("TestFailed");
+            System.out.println("Test passed");
+        }
+        catch(Exception e){
+            System.out.println("Exception occured"+ e.getMessage());
+        }
+    }
+}
